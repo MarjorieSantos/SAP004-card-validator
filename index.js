f701384173620c852f9e870d6ed44eadd5f1ca0f
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-04-07 19:43:42 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,31 @@
+import validator from './validator.js';
+
+let numCard = document.getElementById("cartao")
+let showNumCard = document.getElementById("showNumCard")
+let validar = document.getElementById("btn-validar")
+let result = document.getElementById("result")
+let numCardNoMask = ""
+
+
+numCard.oninput = function(event){
+    showNumCard.innerHTML = validator.maskify(numCard.value)  
+}
+
+validar.onclick = function(){
+    console.log(numCard.value)
+    if (numCard.value == ""){
+        alert('Digite os 16 números da tarjeta')
+    } else{
+        if(validator.isValid(numCard.value)){
+            result.innerHTML = "Valid Card Number"
+        } else{
+            result.innerHTML = "Invalid Card Number"
+        }
+        result.style.display = "block"
+    }
+}
+
+
+
+
+
---
+++ validator.js
@@ -0,0 +1,50 @@
+const validator = {
+  isValid: function (creditCardNumber) {
+    let reverse = Array.from(creditCardNumber).reverse()
+    let numPares = []
+    let numImpar = []
+    let newArray = []
+    for (let i = 1; i < reverse.length; i += 2) {
+      let doubleNumber = reverse[i] * 2
+
+      if (doubleNumber >= 10) {
+        let numeroSeparado = Array.from(doubleNumber.toString())
+        let newNumber = parseInt(numeroSeparado[0]) + parseInt(numeroSeparado[1])
+        numPares.push(newNumber)
+      } else {
+        numPares.push(doubleNumber)
+      }
+    }
+
+    for (let i = 0; i < reverse.length; i += 2) {
+      numImpar.push(parseInt(reverse[i]))
+    }
+
+    newArray = [...numPares, ...numImpar]
+
+    let sum = 0;
+    for (let i = 0; i < newArray.length; i++) {
+      sum += newArray[i]
+    }
+
+    return sum % 10 == 0
+  },
+
+  maskify: function (creditCardNumber) {
+    let maskifyNumber = ""
+    for (let i = 0; i < creditCardNumber.length - 4; i++) {
+      maskifyNumber += '#';
+    }
+
+    for (var j = 4; j >= 1; j--) {
+      var lastCharacter = creditCardNumber.charAt(creditCardNumber.length - j);
+      var lastNums = lastCharacter;
+      maskifyNumber += lastNums;
+    }
+
+    return maskifyNumber
+  }
+
+};
+
+export default validator;
