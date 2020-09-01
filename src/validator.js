85117501c84b261585bce90202f9575a066fe4cb
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-09-01 14:06:49 -0300

    mudando o nome de algumas variaveis

--- src/validator.js
+++ src/validator.js
@@ -1,56 +1,52 @@
 const validator = {
   isValid: function (creditCardNumber) {
     let reverse = Array.from(creditCardNumber).reverse()
-    let numPares = []
-    let numImpar = []
+    let numPairs = []
+    let numOdd = []
     let newArray = []
     for (let i = 1; i < reverse.length; i += 2) {
       let doubleNumber = reverse[i] * 2
 
       if (doubleNumber >= 10) {
-        let numeroSeparado = Array.from(doubleNumber.toString())
-        let newNumber = parseInt(numeroSeparado[0]) + parseInt(numeroSeparado[1])
-        numPares.push(newNumber)
+        let separateNumber = Array.from(doubleNumber.toString())
+        let newNumber = parseInt(separateNumber[0]) + parseInt(separateNumber[1])
+        numPairs.push(newNumber)
       } else {
-        numPares.push(doubleNumber)
+        numPairs.push(doubleNumber)
       }
     }
 
     for (let i = 0; i < reverse.length; i += 2) {
-      numImpar.push(parseInt(reverse[i]))
+      numOdd.push(parseInt(reverse[i]))
     }
-    newArray = [...numPares, ...numImpar]
-    
+    newArray = [...numPairs, ...numOdd]
+
     let sum = 0;
     for (let i = 0; i < newArray.length; i++) {
       sum += newArray[i]
     }
-
     return sum % 10 == 0
   },
 
   maskify: function (creditCardNumber) {
     let maskifyNumber = ""
     let separator = " "
-    let verifica = 0
+    let check = 0
     for (let i = 0; i < creditCardNumber.length - 4; i++) {
-      verifica++
+      check++
       maskifyNumber += "#"
-      if((verifica % 4) == 0){
+      if ((check % 4) == 0) {
         maskifyNumber += separator
       }
-
     }
 
-    for (var j = 4; j >= 1; j--) {
-      var lastCharacter = creditCardNumber.charAt(creditCardNumber.length - j);
-      var lastNums = lastCharacter;
-      maskifyNumber += lastNums;
+    for (let j = 4; j >= 1; j--) {
+      let lastCharacter = creditCardNumber.charAt(creditCardNumber.length - j);
+      let lastNumber = lastCharacter;
+      maskifyNumber += lastNumber;
     }
-
     return maskifyNumber
   }
-
 };
 
 export default validator;
